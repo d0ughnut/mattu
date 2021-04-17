5b14eab608853dcbb7733a9bf6a36cbfce25baa1
root <root@instance-1.us-west1-b.c.doughnut.internal>
2021-04-18 03:43:49 +0900

    update page design.

--- docs/index.php
+++ docs/index.php
@@ -2,6 +2,7 @@
 
 <?php
   # set server root path.
+  # it's not necessary if you can set environment variables directly as a server user.
   putenv("SERVER_ROOT=/var/www");
 ?>
 
@@ -11,40 +12,67 @@
 
   <title>mattu</title>
 
-  <link rel="stylesheet" href="css/style.css?v=1.0">
-  <script src="js/script.js"></script>
+  <link rel="stylesheet" href="css/style.css">
+  <!-- <script src="js/script.js"></script> -->
 
   <!--[if lt IE 9]>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
   <![endif]-->
 </head>
 
-<body>
+<body class="bg">
+
+<div class="disp">
 
 <ul class="tab-container">
   <li>
-    <input class="tab-toggle" id="tab-1" type="radio" name="toggle" checked />
-    <label data-title="Tab 1" class="tab l-b" for="tab-1">ツイート</label>
+    <input class="tab-toggle" id="tab-1" type="radio" name="toggle" checked /> 
+    <label data-title="Tab 1" class="tab l-b" for="tab-1">ツイート</label>            
     <ul class="tab-content-container">
       <li class="tab-content">
+        <h3>状態:</h3>
         <?php
           $server_root = getenv('SERVER_ROOT');
           $msg = file_get_contents("${server_root}/val/countdown");
           echo "<p>${msg}</p>"
         ?>
-        <a href="#" class="btn-flat-simple">
-          <i class="fa fa-caret-right"></i>ツイート
-        </a>
+        <h3>最終ツイート時刻:</h3>
+        <?php
+          $server_root = getenv('SERVER_ROOT');
+          $msg = file_get_contents("${server_root}/val/last_tweet_time");
+          echo "<p>";
+          echo date('Y/m/d G:i:s', ${msg});
+          echo "</p>";
+        ?>
+        <button class="button">呟く@argmattu2</button> 
       </li>
     </ul>
   </li>
   <li>
     <input class="tab-toggle" id="tab-2" type="radio" name="toggle" />
-    <label class="tab" for="tab-2">辞書登録</label>
+    <label class="tab" for="tab-2">辞書登録</label> 
     <ul class="tab-content-container">
       <li class="tab-content">
-        content2
+        <div class="inner contact">
+          <div class="contact-form">
+            <form id="contact-us" method="post" action="#">
+              <div class="user-box">
+                <input type="text" name="" required="">
+                <label>言いそうなことを入力</label>
+              </div>
+              <div class="relative fullwidth col-xs-12">
+                <button type="submit" id="submit" name="submit" class="button">登録</button> 
+              </div>
+              <div class="clear"></div>
+            </form>
+          </div>
+        </div>
       </li>
     </ul>
   </li>
 </ul>
+
+</div>
+
+</body>
+</html>
